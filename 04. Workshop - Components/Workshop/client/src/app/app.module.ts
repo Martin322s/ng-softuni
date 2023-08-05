70b0d4bfaf880c003eda7ea7607f7aa75cd8d71c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-05 18:42:29 +0300

    add aside component

--- 04. Workshop - Components/Workshop/client/src/app/app.module.ts
+++ 04. Workshop - Components/Workshop/client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
