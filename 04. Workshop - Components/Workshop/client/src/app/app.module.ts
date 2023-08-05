aa2a25e2c3158838e28e8308eac0e92883292bfd
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-05 18:36:02 +0300

    footer component

--- 04. Workshop - Components/Workshop/client/src/app/app.module.ts
+++ 04. Workshop - Components/Workshop/client/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, FooterComponent]
 })
 export class AppModule { }
