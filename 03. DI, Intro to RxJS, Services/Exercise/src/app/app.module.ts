a9d6edcb208b778f322bb233101861f6c7d29544
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-06 17:44:04 +0300

    liskov sub principle

--- 03. DI, Intro to RxJS, Services/Exercise/src/app/app.module.ts
+++ 03. DI, Intro to RxJS, Services/Exercise/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { LiskComponent } from './lisk/lisk.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    LiskComponent
   ],
   imports: [
     BrowserModule
