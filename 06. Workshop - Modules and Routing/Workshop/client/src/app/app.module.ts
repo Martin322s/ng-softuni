0afc11f1734d0bb7f1d17e7cca282c3505001867
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-09 00:26:29 +0300

    workshop routing assets and resources

---
+++ 06. Workshop - Modules and Routing/Workshop/client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    AsideComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    ApiService
+  ],
+  bootstrap: [AppComponent, FooterComponent]
+})
+export class AppModule { }
