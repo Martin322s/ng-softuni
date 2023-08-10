20e7863019e4b0e1d08fec96886cae73fea87777
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-10 12:25:35 +0300

    clear users functionality

--- 06. Workshop - Modules and Routing/Workshop/client/src/app/header/header.component.ts
+++ 06. Workshop - Modules and Routing/Workshop/client/src/app/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  loggedIn: true | undefined
+  loggedIn: false | undefined
 }
--- 06. Workshop - Modules and Routing/Workshop/client/src/app/theme-details/theme-details.component.ts
+++ 06. Workshop - Modules and Routing/Workshop/client/src/app/theme-details/theme-details.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./theme-details.component.css']
 })
 export class ThemeDetailsComponent {
-
+  loggedIn: false | undefined;
 }
