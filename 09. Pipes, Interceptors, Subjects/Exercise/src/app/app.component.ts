8429dd87717a3722ea62cd6d770df56f7cc168c1
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-12 19:36:15 +0300

    pipes exercise

--- 09. Pipes, Interceptors, Subjects/Exercise/src/app/app.component.ts
+++ 09. Pipes, Interceptors, Subjects/Exercise/src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { interval, map } from 'rxjs';
+
+function add(a: number | string, b: number | string): number | string {
+    return (a as any) + (b as any);
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Exercise';
+    title = 'Pipes-interceptors';
+
+    obj = {
+        // prop1: 'Hello world',
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    };
+
+    add = add;
+
+    private scores: number[] = [];
+    private result = 0;
+
+    getProp(obj: any, propName: string) {
+        return obj[propName]
+    }
+
+    calcScore(obj: { scores: number[] }) {
+        if (this.scores !== this.obj.scores) {
+            this.result = obj.scores.reduce((acc, curr) => acc + curr);
+            this.scores = obj.scores;
+            return this.result;
+        } else {
+            return this.result;
+        }
+    }
+
+    addProp() {
+        (this.obj as any)['test'] = 500;
+    }
+
+    $time = interval(1000).pipe(
+        map(() => new Date())
+    )
+    // myPromise = new Promise((res) => {
+    //     setTimeout(() => {
+    //         res('Hello from promise')
+    //     }, 5000);
+    // })
 }
