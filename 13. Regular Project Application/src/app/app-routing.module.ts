b2ca6e53006b6b97254436ad900ac72dabbebaef
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-14 00:27:43 +0300

    add autocomplete tags for login and register

--- 13. Regular Project Application/src/app/app-routing.module.ts
+++ 13. Regular Project Application/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateFactComponent } from './create-fact/create-fact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'facts', pathMatch: 'full', component: DashboardComponent },
-    { path: 'create-fact', pathMatch: 'full', component: CreateFactComponent }
+    { path: 'create-fact', pathMatch: 'full', component: CreateFactComponent },
+    { path: 'login', pathMatch: 'full', component: LoginComponent },
+    { path: 'register', pathMatch: 'full', component: RegisterComponent }
 ];
 
 @NgModule({
