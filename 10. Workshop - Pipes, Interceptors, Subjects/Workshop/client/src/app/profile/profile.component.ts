7ec180e306393d9dc03ccf1dfc778cd0e19fb75b
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-13 14:07:41 +0300

    render user information in the profile page

--- 10. Workshop - Pipes, Interceptors, Subjects/Workshop/client/src/app/profile/profile.component.ts
+++ 10. Workshop - Pipes, Interceptors, Subjects/Workshop/client/src/app/profile/profile.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AuthService } from '../services/authService';
 import { getToken } from '../utils/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnDestroy {
   data: any;
+  subscription: Subscription;
+  
   constructor(private authService: AuthService) {
     const token = getToken();
-    this.authService.getProfile(token).subscribe({
-      next: (res) => this.data = res,
+    this.subscription = this.authService.getProfile(token).subscribe({
+      next: (res) => {
+        this.data = res;
+      },
       error: (err) => console.log(err)
     });
-    console.log(this.data);
+  }
+  
+  getData() {
+    return this.data;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
