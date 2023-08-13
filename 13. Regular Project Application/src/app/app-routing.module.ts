d25636322150781ee1ec13891d9390676f8dcefa
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-14 00:21:14 +0300

    render dashboard and create page

--- 13. Regular Project Application/src/app/app-routing.module.ts
+++ 13. Regular Project Application/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateFactComponent } from './create-fact/create-fact.component';
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', component: HomeComponent }
+    { path: '', pathMatch: 'full', component: HomeComponent },
+    { path: 'facts', pathMatch: 'full', component: DashboardComponent },
+    { path: 'create-fact', pathMatch: 'full', component: CreateFactComponent }
 ];
 
 @NgModule({
