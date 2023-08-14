5495305201439460de606d8c7b9604d62a6c93cd
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-14 12:16:08 +0300

    change guests/user validiation

--- 13. Regular Project Application/src/app/header/header.component.ts
+++ 13. Regular Project Application/src/app/header/header.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isLoggedIn: boolean = false;
 
+  constructor() {
+    let user: any = localStorage.getItem('session');
+    if (user !== null) {
+      user = JSON.parse(user);
+    } else {
+      user = {};
+    }
+
+    if (user?.accessToken) {
+      this.isLoggedIn = true;
+    }
+  }
 }
