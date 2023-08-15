e3b19bdc4d7b8191e3d632b24ddaee4a89da69f7
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-15 12:09:24 +0300

    change nav depending on router event

--- 13. Regular Project Application/src/app/auth-service.service.ts
+++ 13. Regular Project Application/src/app/auth-service.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   apiUrl: string = 'http://localhost:3030/users';
-  isAuthenticated: boolean = false;
-
+  public isAuthenticatedSubject = new BehaviorSubject<Boolean>(false);
+  public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
+  
   constructor(private http: HttpClient) { }
 
-  isLoggedIn() { return this.isAuthenticated }
   loginUser(userData: { email: string, password: string }) {
-    this.isAuthenticated = true;
     return this.http.post(`${this.apiUrl}/login`, userData, {
       headers: {
         'Content-Type': 'application/json'
--- 13. Regular Project Application/src/app/header/header.component.ts
+++ 13. Regular Project Application/src/app/header/header.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth-service.service';
+import { BehaviorSubject } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {;
-  isLoggedIn: boolean = false;
-  constructor(private authService: AuthService) {
-    this.isLoggedIn = this.authService.isLoggedIn();
-  }
+export class HeaderComponent implements OnInit {
+    isAuthenticated: Boolean = false;
 
-  LoggedIn() {
-    return this.isLoggedIn;
-  }
+    constructor(private authService: AuthService, private router: Router) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                let stat = localStorage.getItem('status');
+                if (stat !== null) {
+                    let status = JSON.parse(stat)
+                    this.authService.isAuthenticatedSubject.next(status);
+                    this.authService.isAuthenticated$.subscribe(isAuth => {
+                        this.isAuthenticated = isAuth;
+                    });
+                }
+            }
+        })
+
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    LoggedIn() {
+        return this.isAuthenticated;
+    }
 }
