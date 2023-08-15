29b085be132da0c7972c8efde305f417459c7d2d
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-15 13:21:05 +0300

    register user

--- 13. Regular Project Application/src/app/auth-service.service.ts
+++ 13. Regular Project Application/src/app/auth-service.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   apiUrl: string = 'http://localhost:3030/users';
   public isAuthenticatedSubject = new BehaviorSubject<Boolean>(false);
   public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
-  
+
   constructor(private http: HttpClient) { }
 
   loginUser(userData: { email: string, password: string }) {
@@ -22,5 +22,13 @@ export class AuthService {
 
   logoutUser() {
     return this.http.get(`${this.apiUrl}/logout`);
+  }
+
+  registerUser(userData: { email: string, password: string }) {
+    return this.http.post(`${this.apiUrl}/register`, userData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   }
 }
--- 13. Regular Project Application/src/app/register/register.component.ts
+++ 13. Regular Project Application/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private authService: AuthService, private router: Router) { }
 
+  submitHandler(form: NgForm) {
+    if (Object.values(form.value).some(x => x === '')) {
+      alert('All fields must be filled correctly!');
+    } else {
+      this.authService.registerUser(form.value).subscribe({
+        next: (res) => {
+          localStorage.setItem('session', JSON.stringify(res));
+          localStorage.setItem('status', 'true');
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+    }
+  }
 }
