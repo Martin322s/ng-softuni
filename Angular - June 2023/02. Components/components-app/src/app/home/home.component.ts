222ae08967ea8d0635e9e885aef80d4cc4f25007
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-10 12:52:07 +0200

    attach click event

--- Angular - June 2023/02. Components/components-app/src/app/home/home.component.ts
+++ Angular - June 2023/02. Components/components-app/src/app/home/home.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+    handleClick() {
+        console.log('clicked');
+    }
 }
