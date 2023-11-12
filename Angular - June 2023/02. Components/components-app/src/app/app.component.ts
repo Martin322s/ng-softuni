682de890939d4ab4a64749b8db8e7808a881fe21
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-12 11:43:58 +0200

    clear application

--- Angular - June 2023/02. Components/components-app/src/app/app.component.ts
+++ Angular - June 2023/02. Components/components-app/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, HomeComponent]
+    imports: [CommonModule, RouterOutlet]
 })
 
 export class AppComponent {
