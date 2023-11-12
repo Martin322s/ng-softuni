b819060f26e8b14464bb5fc1f35586e59075290e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-12 12:31:51 +0200

    attach events

--- Angular - June 2023/02. Components/components-app/src/app/home/home.component.ts
+++ Angular - June 2023/02. Components/components-app/src/app/home/home.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  games: { name: string, year: number}[] = [{ name: 'Lol', year: 2009 }, { name: 'WWE 2K17', year: 2016 }]
+    render: boolean = false;
+    games: { name: string, year: number }[] = [{ name: 'Lol', year: 2009 }, { name: 'WWE 2K17', year: 2016 }]
+
+    renderGames(): void {
+        this.render = !this.render;
+    }
 }
