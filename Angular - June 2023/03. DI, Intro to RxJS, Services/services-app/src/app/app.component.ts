0c4ba4b71f6ea171e2fc43fb180a985f1704ce65
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-13 13:51:09 +0200

    change detection strategy

--- Angular - June 2023/03. DI, Intro to RxJS, Services/services-app/src/app/app.component.ts
+++ Angular - June 2023/03. DI, Intro to RxJS, Services/services-app/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, UserListComponent],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'services-app';
+    users: { name: string; age: number; }[] = [
+        { name: 'Peter', age: 22 },
+        { name: 'Martin', age: 23 },
+        { name: 'Mitko', age: 23 },
+        { name: 'Ivan', age: 24 }
+    ];
+
+    addNewUser(name: HTMLInputElement, age: HTMLInputElement) {
+        const user = { name: name.value, age: Number(age.value) };
+        this.users = [...this.users, user];
+
+        name.value = '';
+        age.value = '';
+    }
 }
---
+++ Angular - June 2023/03. DI, Intro to RxJS, Services/services-app/src/app/user-list-item/user-list-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-list-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-list-item.component.html',
+  styleUrl: './user-list-item.component.css'
+})
+export class UserListItemComponent {
+  @Input() user: { name: string, age: number } | undefined = undefined;
+}
