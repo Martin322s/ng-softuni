3b6a2c3484614c3bfc8e1dadd44ed2b45433a11e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-16 11:45:28 +0200

    promise vs observable

--- Angular - June 2023/03. DI, Intro to RxJS, Services/services-app/src/app/app.component.ts
+++ Angular - June 2023/03. DI, Intro to RxJS, Services/services-app/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './user.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [CommonModule, RouterOutlet, UserListComponent],
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [UserService]
 })
 export class AppComponent {
     title = "Application works!";
@@ -24,3 +26,21 @@ export class AppComponent {
         this.appUsers = this.userService.users;
     }
 }
+
+const p = new Promise((resolve, reject) => {
+    console.log('from promise involved');
+    
+    setTimeout(() => {
+        resolve(1200);
+    }, 4000);
+});
+
+p.then((data) => console.log('promise', data));
+
+const o = new Observable(observer => {
+    observer.next(1000);
+    observer.next(1001);
+    observer.next(1002);
+});
+
+o.subscribe(data => console.log('observer - ', data));
