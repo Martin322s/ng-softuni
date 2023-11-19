6f8fc6044b88f9ceca2bf52992deba379719a9b0
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-19 15:48:53 +0200

    setup core module

--- Angular - June 2023/04. Workshop - Components/workshop-components/src/app/core/core.module.ts
+++ Angular - June 2023/04. Workshop - Components/workshop-components/src/app/core/core.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  // Which components to be exported from this module
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
