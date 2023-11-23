36dae30b5d94948ae49388688194e6d78d55f2a6
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-23 11:40:16 +0200

    separation of modules

--- Angular - June 2023/05. Modules and Routing/routing-app/src/app/app.module.ts
+++ Angular - June 2023/05. Modules and Routing/routing-app/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { GlobalLoaderComponent } from './core/global-loader/global-loader.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
