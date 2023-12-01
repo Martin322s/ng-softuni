bad367d7e8a5766ed91713faf81823058b36bb83
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-12-01 18:56:26 +0200

    generate todo module

--- Angular - June 2023/05. Modules and Routing/routing-app/src/app/app.module.ts
+++ Angular - June 2023/05. Modules and Routing/routing-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { UsersModule } from './users/users.module';
 import { HttpClientModule } from '@angular/common/http';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     CoreModule,
     UsersModule,
-    HttpClientModule
+    HttpClientModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
