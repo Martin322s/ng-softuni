2658d0d7887549ef0fc89fd67ef142a2ea6660bc
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-12-07 14:02:42 +0200

    register user

--- Angular - June 2023/06. Workshop Routing/workshop-routing/src/app/user/register/register.component.ts
+++ Angular - June 2023/06. Workshop Routing/workshop-routing/src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private userService: UserService, private router: Router) {
 
+  }
+  register(event: MouseEvent, username: string, email: string, phone:string, password: string, rePassword: string): void {
+    event.preventDefault();
+    this.userService.login();
+    this.router.navigate(['/']);
+  }
 }
