9ae49271bc104208fe1a3597deb2810b7d041299
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-29 12:06:23 +0200

    ex-3

---
+++ Angular - June 2023/01. Intro to Angular and Typescript/intro-to-angular-and-ts/typescript/print-and-sum.ts
@@ -0,0 +1,14 @@
+function solve(start: number, end: number) {
+    let startNum: number = start;
+    let endNum: number = end;
+    let output: string = "";
+    let sum: number = 0;
+
+    for (let i: number = startNum; i <= endNum; i++) {
+        output += `${i} `;
+        sum += i;
+    }
+    
+    console.log(output);
+    console.log(`Sum: ${sum}`);
+}
