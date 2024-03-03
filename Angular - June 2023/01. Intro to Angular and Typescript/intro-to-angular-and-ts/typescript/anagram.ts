7656d5f8b085ca3c7a9c89f0f294ca1ef2eb3a96
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-03-03 12:32:49 +0200

    ex-6

---
+++ Angular - June 2023/01. Intro to Angular and Typescript/intro-to-angular-and-ts/typescript/anagram.ts
@@ -0,0 +1,5 @@
+function isAnagram(str1: string, str2: string): boolean {
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+    return sortedStr1 === sortedStr2;
+}
