b7e5adedb7afe43efd0f2809d0a48d762af25f68
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-03-06 12:39:00 +0200

    ex-3

---
+++ Angular - November 2022/01. Intro to Angular and TypeScript/Exercises/tsc/fibonacci-series.ts
@@ -0,0 +1,7 @@
+function fibonacciSeries(n: number): number[] {
+    const series: number[] = [0, 1];
+    for (let i = 2; i < n; i++) {
+        series.push(series[i - 1] + series[i - 2]);
+    }
+    return series;
+}
