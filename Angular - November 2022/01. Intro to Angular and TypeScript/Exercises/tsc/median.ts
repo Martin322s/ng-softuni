24420a0e1a31ace6e245214e5c2d7f0201d7b987
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-03-06 12:42:11 +0200

    ex-9

---
+++ Angular - November 2022/01. Intro to Angular and TypeScript/Exercises/tsc/median.ts
@@ -0,0 +1,9 @@
+function median(numbers: number[]): number {
+    const sorted = numbers.slice().sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    } else {
+        return sorted[middle];
+    }
+}
